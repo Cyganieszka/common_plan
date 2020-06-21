2644b6594ea38a4b717848f9f94705e9aeb547d6
cyganieszka <agnieszka.koczuba@gmail.com>
2020-06-21 16:21:15 +0200

    all day events slot

--- lib/timeline/time_listview.dart
+++ lib/timeline/time_listview.dart
@@ -122,6 +122,7 @@ class TimeListView extends StatelessWidget{
       children: [
         if(allDaySlots>0)Container(
           height: (allDaySlots*30.0),
+          decoration: BoxDecoration(color:Colors.grey.withAlpha(100)),
           child: Column(
             children: allDayWidgets.map((e) => Expanded(child: e,flex: 1,)).toList(),
           ),
